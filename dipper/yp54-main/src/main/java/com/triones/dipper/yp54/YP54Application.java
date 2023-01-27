d6ea5cf2de467af3d0546ab2c657c1ba6d404281
fwj <fengwenjin0218@163.com>
2023-01-28 04:42:13 +0800

    init yp54 module

---
+++ dipper/yp54-main/src/main/java/com/triones/dipper/yp54/YP54Application.java
@@ -0,0 +1,13 @@
+package com.triones.dipper.yp54;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YP54Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YP54Application.class, args);
+    }
+
+}
